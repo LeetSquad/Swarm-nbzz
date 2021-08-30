596e55629c97b59ec4337f42d5d6d4d0f4c9c3b6
anonymous <anony@mous.com>
2021-08-30 16:50:15 +0800

    Reduce gas limit

--- nbzz/cmds/start.py
+++ nbzz/cmds/start.py
@@ -28,7 +28,7 @@ def start_cmd(ctx: click.Context, password,bee_key_path) -> None:
     my_local_acc=w3.eth.account.from_key(privatekey)
     w3.eth.default_account=my_local_acc.address
     nbzz_contract=w3.eth.contract(address=config["network_overrides"]["constants"][config["selected_network"]]["CONTRACT"],abi=NBZZ_ABI)
-    construct_txn = nbzz_contract.functions.nodeOnline(w3.eth.default_account,w3.eth.default_account).buildTransaction({"nonce":w3.eth.getTransactionCount(my_local_acc.address),"gas":2900_0000}) #start
+    construct_txn = nbzz_contract.functions.nodeOnline(w3.eth.default_account,w3.eth.default_account).buildTransaction({"nonce":w3.eth.getTransactionCount(my_local_acc.address),"gas":290_0000}) #start
     print(construct_txn)
     signed =my_local_acc.sign_transaction(construct_txn)
     tx_hash = w3.eth.sendRawTransaction(signed.rawTransaction)
